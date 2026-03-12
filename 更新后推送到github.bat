ab8758d6f8305fd6ad73696bafd74eb03302fe50
junmunicorns-lgtm <junmunicorns@gmail.com>
2026-03-12 12:31:52 +0800

    update

--- 更新后推送到github.bat
+++ 更新后推送到github.bat
@@ -1,8 +1,11 @@
-@echo off
+@echo on
 cd /d D:\KOCdata
 if exist .git\index.lock del .git\index.lock
+echo 正在添加文件...
 git add .
+echo 正在提交...
 git commit -m "update"
+echo 正在推送...
 git push
-echo done
+echo 完成！
 pause
